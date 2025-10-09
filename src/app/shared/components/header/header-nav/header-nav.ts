3ae5412409c723c2ebb20f0764b7568edc839c40
Ahmed Rashad Jamion <jamionahmedrashadi@gmail.com>
2025-10-09 21:05:26 +0800

    Improved nav

--- src/app/shared/components/header/header-nav/header-nav.ts
+++ src/app/shared/components/header/header-nav/header-nav.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Icon } from '../../icon/icon';
 
 @Component({
   selector: 'app-header-nav',
-  imports: [],
+  imports: [Icon],
   templateUrl: './header-nav.html',
-  styleUrl: './header-nav.css'
+  styleUrl: './header-nav.css',
 })
 export class HeaderNav {
+  isNavOpen = signal(false);
 
+  toggleNav() {
+    this.isNavOpen.update((isOpen) => !isOpen);
+  }
 }
